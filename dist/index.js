08c7ea50ef5022110bcb89e7d1d16b2d3921c06f
Josman <josemanuek@hotmail.com>
2025-03-25 17:25:31 -0600

    testing node api for odoo

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("WELCOME  to Node JS with Typescript & Express");
+});
+app.post("/login", (req, res) => {
+    try {
+        setTimeout(() => {
+            res.send({
+                "user": req.body.user,
+                "odoo_id": 310696,
+                "name": "FULANITO PEREZ",
+                "branch": [
+                    50,
+                    "LA PIMIENTA"
+                ],
+                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImxvZ2luIjoiUmV5bmFNZXJjZWRlcyIsImVtYWlsIjoicm15YW0iLCJsb2FuX29mZmljZXIiOjMxMDY5NiwibmFtZSI6IlJFWU5BIE1FUkNFREVTIiwibGFzdG5hbWUiOiJZQU0iLCJzZWNvbmRfbGFzdG5hbWUiOiJCQUxBTiIsImJyYW5jaCI6WzUwLCJDQU1QRUNIRSJdLCJvZmZpY2VyX3JhbmsiOltudWxsLCIiXX0sInN5bmNfaW5mbyI6eyJsb2NhbF90YXJnZXQiOiJsb2NhbC1kYiIsInJlbW90ZV90YXJnZXQiOiJjbnNydi1wcm9tb3RvciIsInN5bmNfZXhwaXJhdGlvbiI6IjIwMjUtMDMtMjZUMTg6MjU6MzguMzI5WiJ9LCJpYXQiOjE3NDI5MjcxMzh9.AQCSq1jauK_yVVLODT-bMK3zGDe7zBHzLEr6AwiamSk"
+            });
+        }, 2000);
+    }
+    catch (e) {
+        res.send(e.message);
+    }
 });
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 
-import express, { Express, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import "dotenv/config";
 
 const app = express();
-
+app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
   res.send("WELCOME  to Node JS with Typescript & Express");
 });
+
+app.post("/login", (req, res) => {
+  try {
+
+    setTimeout(() => {
+      res.send({
+
+        "user": req.body.user,
+        "odoo_id": 310696,
+        "name": "FULANITO PEREZ",
+        "branch": [
+          50,
+          "LA PIMIENTA"
+        ],
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImxvZ2luIjoiUmV5bmFNZXJjZWRlcyIsImVtYWlsIjoicm15YW0iLCJsb2FuX29mZmljZXIiOjMxMDY5NiwibmFtZSI6IlJFWU5BIE1FUkNFREVTIiwibGFzdG5hbWUiOiJZQU0iLCJzZWNvbmRfbGFzdG5hbWUiOiJCQUxBTiIsImJyYW5jaCI6WzUwLCJDQU1QRUNIRSJdLCJvZmZpY2VyX3JhbmsiOltudWxsLCIiXX0sInN5bmNfaW5mbyI6eyJsb2NhbF90YXJnZXQiOiJsb2NhbC1kYiIsInJlbW90ZV90YXJnZXQiOiJjbnNydi1wcm9tb3RvciIsInN5bmNfZXhwaXJhdGlvbiI6IjIwMjUtMDMtMjZUMTg6MjU6MzguMzI5WiJ9LCJpYXQiOjE3NDI5MjcxMzh9.AQCSq1jauK_yVVLODT-bMK3zGDe7zBHzLEr6AwiamSk"
+
+      })
+    }, 2000)
+  }
+  catch (e: any) {
+    res.send(e.message);
+  }
+})
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
